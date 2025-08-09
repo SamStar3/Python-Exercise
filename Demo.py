3761cf64d90827dac55c3ab020529cfd9def8756
samstar3 <samstar1044948@gmail.com>
2025-08-09 23:04:13 +0530

    file added

--- Demo.py
+++ Demo.py
@@ -1,3 +1,11 @@
 print("Hello World!")
 print("This is a demo script.")
 
+print ('*' * 20)
+
+price = 100
+price = 200
+print("The price is:", price)
+
+possible = True
+print("Is it possible?", possible)
