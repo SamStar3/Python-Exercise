5a3633e03324c238916e647021bbb1125c9c38a0
samstar3 <samstar1044948@gmail.com>
2025-08-10 20:22:22 +0530

    file added

--- hospital.py
+++ hospital.py
@@ -2,7 +2,15 @@
 # age = 30
 # is_new_patient = True
 
-name = input('Enter your name :')
+# name = input('Enter your name :')
+# fav_color = input('Enter your favorite color :')
 
-print('Hi ' + name + '!')
+# print('Hi ' + name + '!')
 
+# print(name + ' likes ' + fav_color + '.')
+
+brith_year = input('Enter your birth year :')
+
+age = 2025 - int(brith_year)
+
+print(age)
