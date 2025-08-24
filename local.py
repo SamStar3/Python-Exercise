099ebff80d9fba66e1eb730047b678069542baf0
samstar3 <samstar1044948@gmail.com>
2025-08-24 21:21:50 +0530

    file added

--- local.py
+++ local.py
@@ -1,1 +1,14 @@
 print("Demo")
+
+secret_number = 9
+i = 3
+# count = 0
+# limit = 3
+
+while i > 0:
+    guess = int(input("Enter your guess: "))
+    if guess == secret_number:
+        print("you win")
+        break
+    else:
+        print("try again")
