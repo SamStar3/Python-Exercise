774416bc60d940f03d28900580349e30d442a9f0
samstar3 <samstar1044948@gmail.com>
2025-08-27 21:32:54 +0530

    file added

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-fruit = "Apple"
-print(fruit[1:-1])
+# fruit = "Apple"
+# print(fruit[1:-1])
 
-x = 3 ** 3
-print(x)
+# x = 3 ** 3
+# print(x)
 
-x = 10 ** 3
-print(x)
+# x = 10 ** 3
+# print(x)
+
+
+def fun(n):
+    return int(str(int('1'n))*2 == ('1'*n) * (n*2 - 1).count('1'))
+
+print(fun(3))
